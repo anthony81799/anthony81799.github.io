b8e87ae098b6fadb12288b5debe331624ae4690a
Anthony Mason <anthony81799@gmail.com>
2025-04-19 13:50:43 -0400

    Fix Github Pages Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/anthony81799.github.io",
   plugins: [react()],
+  esbuild: {
+    loader: "jsx"
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      }
+    }
+  }
 })
