39f8489bb70f203ad5a18ff2310cf89b3122fee0
Anthony Mason <anthony81799@gmail.com>
2025-04-19 14:06:34 -0400

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/anthony81799.github.io",
+  base: "/",
   plugins: [react()],
   esbuild: {
     loader: "jsx"
